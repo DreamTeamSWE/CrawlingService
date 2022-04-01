fb463153673f7ae73199619cc614007271fefaff
mikigatto93 <mikigatto93@gmail.com>
2022-04-02 00:21:04 +0200

    aggiunto to_json a Location e CrawledData

--- crawler/CrawledData.py
+++ crawler/CrawledData.py
@@ -1,12 +1,27 @@
+import json
+
 from instagrapi.types import Media
 from crawler.Location import Location
 
+
 class CrawledData:
 
-    def __init__(self, username, post_id, date, img_url: list[str], caption_text: str, location: Location = None) -> None:
+    def __init__(self, username, post_id, date, img_url: list[str], caption_text: str,
+                 location: Location = None) -> None:
         self.__username = username
         self.__post_id = post_id
         self.__date = date
         self.__img_url = img_url
         self.__caption_text = caption_text
         self.__location = location
+
+    def to_json(self) -> str:
+        json_obj = {
+            "username": self.__username,
+            "post_id": self.__post_id,
+            "date": self.__date,
+            "img_url": self.__img_url,
+            "caption_text": self.__caption_text,
+            "location": None if None else self.__location.to_json()
+        }
+        return json.dumps(json_obj)
--- crawler/Location.py
+++ crawler/Location.py
@@ -1,13 +1,28 @@
+import json
+
+
 class Location:
     def __init__(self, locationName, lat, lng, category, phone, website) -> None:
         self.__locationName = locationName  # nome della location del post
         self.__lat = lat  # latitudine della location del post
         self.__lng = lng  # longitudine della location del post
-        self.__category = category #categoria della location
-        self.__phone = phone #numero di telefono della location
-        self.__website = website #sito web della location
+        self.__category = category  # categoria della location
+        self.__phone = phone  # numero di telefono della location
+        self.__website = website  # sito web della location
+
     def is_restaurant(self) -> bool:
-        resaurant_tags = ['Restaurant', 'Bar', 'Grocery ', 'Wine'] #da aggiungere se ne troviamo altri
+        resaurant_tags = ['Restaurant', 'Bar', 'Grocery ', 'Wine']  # da aggiungere se ne troviamo altri
         for tag in resaurant_tags:
             if tag in self.__category: return True
         return False
+
+    def to_json(self) -> str:
+        json_obj = {
+            "locationName": self.__locationName,
+            "lat": self.__lat,
+            "lng": self.__lng,
+            "category": self.__category,
+            "phone": self.__phone,
+            "website": self.__website
+        }
+        return json.dumps(json_obj)
