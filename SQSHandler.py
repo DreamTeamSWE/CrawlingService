23b124df640152a22e701f0ebb1f396ef2b0204c
mikigatto93 <mikigatto93@gmail.com>
2022-04-02 17:49:28 +0200

    mini fix

--- SQSHandler.py
+++ SQSHandler.py
@@ -1,5 +1,4 @@
 import uuid
-import json
 
 import boto3
 
@@ -10,11 +9,12 @@ class SQSHandler:
 
     def __init__(self, queue_name: str) -> None:
         self.__sqs_client = boto3.client('sqs')
-        self.__queue_url = self.__sqs_client.get_queue_url(QueueName=queue_name)
+        response = self.__sqs_client.get_queue_url(QueueName=queue_name)
+        self.__queue_url = response['QueueUrl']
         self.__group_id = uuid.uuid4()
 
     def enqueue_message(self, crawled_data_obj: CrawledData) -> dict:
         response = self.__sqs_client.send_message(QueueUrl=self.__queue_url,
-                                                  MessageBody=json.dumps(crawled_data_obj.to_json()),
+                                                  MessageBody=crawled_data_obj.to_json(),
                                                   MessageGroupId=str(self.__group_id))
         return response
