2a26d1a2581050d7334d6bb3b57bf3529ed3ce4d
Francesco Protopapa <francesco.protopapa22@gmail.com>
2022-05-08 17:38:25 +0200

    documentazione ProfilesRepository.py

--- repository/ProfilesRepository.py
+++ repository/ProfilesRepository.py
@@ -8,24 +8,48 @@ class ProfilesRepository:
         self.__db = DatabaseHandler('crawler_test')
 
     def __update_last_time_check(self, username: str):
+        """
+        Update the last time check of a profile.
+
+        :param username: username of the profile to update
+        :return: db response
+        """
         username_param = {'name': 'username', 'value': {'stringValue': username}}
         query = 'update profilo_instagram set data_ultimo_check = current_timestamp where username = :username'
         paramset = [username_param]
         return self.__db.do_write_query(query, paramset)
 
     def select_profile(self, username: str):
+        """
+        Select a profile from the database.
+
+        :param username: the username of the profile to select
+        :return: db data of the profile
+        """
         username_param = {'name': 'username', 'value': {'stringValue': username}}
         query = 'select * from profilo_instagram where username = :username'
         paramset = [username_param]
         return self.__db.do_read_query(query, paramset)
 
     def insert_profile(self, username: str):
+        """
+        Insert a profile in the database.
+
+        :param username: the username of the profile to insert
+        :return: db response
+        """
         username_param = {'name': 'username', 'value': {'stringValue': username}}
         query = 'insert into profilo_instagram (username, level) values (:username, 1)'
         paramset = [username_param]
         return self.__db.do_write_query(query, paramset)
 
     def insert_profiles_list(self, profile_list: List[str]):
+        """
+        Insert a list of profiles in the database.
+
+        :param profile_list: the list of profiles to insert
+        :return: db response
+        """
         paramset = []
         i = 0
         query = 'insert into profilo_instagram (username) values '
@@ -37,6 +61,11 @@ class ProfilesRepository:
         return self.__db.do_write_query(query, paramset)
 
     def get_profile_for_crawling_level_1(self):
+        """
+        Get the profile of level 1 that is not checked for the longest time.
+
+        :return: Profile for crawling level 1
+        """
         query = 'select * from profilo_instagram where level = 1 order by data_ultimo_check, username limit 1'
         response = self.__db.do_read_query(query)
         profile = response[0]['username']
